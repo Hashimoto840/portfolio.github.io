db495df6ab4b9df09209ad075e87b2a9b09823f0
Hashimoto840 <sannon448@gmail.com>
2026-04-27 07:56:23 +0900

    Refactor mouse stalker event listeners

--- js/common.js
+++ js/common.js
@@ -63,23 +63,25 @@ overlay.addEventListener("click", () => {
 });
 
 //マウスストーカー
-const stalker = document.getElementById('mouse-stalker');
-let hovFlag = false;
+const stalker = document.getElementById("mouse-stalker");
 
-window.addEventListener('pointermove', function (e) {
-    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
-});
+if (stalker) {
+  window.addEventListener("pointermove", function (e) {
+    stalker.style.transform =
+      "translate(" + e.clientX + "px, " + e.clientY + "px)";
+  });
 
-const linkElem = document.querySelectorAll('a:not(.no_stick_)');
-for (let i = 0; i < linkElem.length; i++) {
-    linkElem[i].addEventListener('mouseover', function (e) {
-        hovFlag = true;
-        stalker.classList.add('is_active');
+  const linkElem = document.querySelectorAll("a:not(.no_stick_)");
+
+  for (let i = 0; i < linkElem.length; i++) {
+    linkElem[i].addEventListener("mouseover", function () {
+      stalker.classList.add("is_active");
     });
-    linkElem[i].addEventListener('mouseout', function (e) {
-        hovFlag = false;
-        stalker.classList.remove('is_active');
+
+    linkElem[i].addEventListener("mouseout", function () {
+      stalker.classList.remove("is_active");
     });
+  }
 }
 
 //フェード
